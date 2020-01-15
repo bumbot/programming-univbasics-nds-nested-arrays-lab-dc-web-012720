73bd59508671c5f81ca746276c2de6528251aa40
Paul Kim <pkim1295@gmail.com>
2020-01-15 00:44:02 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -56,12 +56,13 @@ def sorted_matrix
     ]
   ]
   
-  
+  grocery_store.sort
 end
 
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
